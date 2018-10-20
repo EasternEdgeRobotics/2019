85889c82c288341391835866396cbf455b90d0f3
Nana <abekah12345@gmail.com>
2018-10-20 12:12:18 -0230

    ports included and throws error

--- raspi/Maestro test/controls.py
+++ raspi/Maestro test/controls.py
@@ -3,17 +3,25 @@ import sys
 
 
 
-## Inits the maestro controller from the library.
-servo = maestro.Controller();
+## This will automatically throw an error if one occurs.
+try:
+    ## Inits the maestro controller from the library.
+    servo = maestro.Controller();
 
 
-## get accel, target and speed values from system command
-accel = sys.argv[1];
-target = sys.argv[2];
-speed = sys.argv[3];
+    ## get accel, target and speed values from system command
+    accel = sys.argv[1];
+    target = sys.argv[2];
+    speed = sys.argv[3];
 
-servo.setAccel(0, accel);
-servo.setTarget(0, target);
-servo.setSpeed(0, speed);
+    ## There are only 2 thruster ports at the momemt.
+    ## Additional ports can be easily appended
+    port = [0,1];
 
-print(servo.getPosition(0));
+    ## Set accel, target and speed for port 0
+    servo.setAccel(port[0], accel);
+    servo.setTarget(port[0], target);
+    servo.setSpeed(port[0], speed);
+
+
+    print(servo.getPosition(0));
