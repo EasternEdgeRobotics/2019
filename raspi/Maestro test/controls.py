ab5535c941bdb6d0027d06b38ef8c25f2601c404
Nana <abekah12345@gmail.com>
2018-10-20 12:00:05 -0230

    simplifications

--- raspi/Maestro test/controls.py
+++ raspi/Maestro test/controls.py
@@ -1,26 +1,19 @@
 import maestro
 import sys
-#import time
 
 
 
+## Inits the maestro controller from the library.
+servo = maestro.Controller();
 
 
-servo = maestro.Controller();                                                   ## Inits the maestro controller from the library.
+## get accel, target and speed values from system command
+accel = sys.argv[1];
+target = sys.argv[2];
+speed = sys.argv[3];
 
-try:
-    print("Control Starting: ");
+servo.setAccel(0, accel);
+servo.setTarget(0, target);
+servo.setSpeed(0, speed);
 
-    accel = sys.argv[1];                                                        ## get accel, target and speed values from system command
-    target = sys.argv[2];
-    speed = sys.argv[3];
-
-    servo.setAccel(0, accel);
-    servo.setTarget(0, target);
-    servo.setSpeed(0, speed);
-
-    print(servo.getPosition(0));
-
-except Exception as e:                                                          ## Get an error and print it
-    print("Type error: " + str(e));
-    servo.close();                                                              
+print(servo.getPosition(0));
