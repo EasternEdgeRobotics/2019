61ddd08c5b7dcf144ac6417b02a37a66bde13ede
vilinfield <vilinfield@gmail.com>
2018-11-03 14:51:45 -0230

    fix formatting and ignore cache files

--- raspi/control.py
+++ raspi/control.py
@@ -1,50 +1,55 @@
-## module to control the thruster using the maestro library
+"""Module to control the thruster using the maestro library."""
 
+# Motor Controller limits measured in maestro units
+# Values should be adjusted so that the center, stops the thrusters;
+# These values typically align with the maestro settings
+MIN_SPEED = 4000
+CENTER_SPEED = 6000
+MAX_SPEED = 8000
 
-
-## Motor Controller limits measured in maestro units
-## Values should be adjusted so that the center, stops the thrusters;
-## These values typically align with the maestro settings
-MIN_SPEED = 4000;
-CENTER_SPEED = 6000;
-MAX_SPEED = 8000;
 
 class Thruster:
-    ## When using motor controllers, the maestro's speed setting can be used to tune the
-    ## responsiveness of the robot.  Low values dampen acceleration, making for a more
-    ## stable robot. High values increase responsiveness, but can lead to a tippy robot.
-    ## Try values around 50 to 100.
-    RESPONSE = 70;
+    """Thruster class."""
 
-    # Pass the maestro controller object and the maestro channel numbers being used
-    # for the thruster's motor controller.
+    # When using motor controllers, the maestro's speed setting can be used to tune the
+    # responsiveness of the robot.  Low values dampen acceleration, making for a more
+    # stable robot. High values increase responsiveness, but can lead to a tippy robot.
+    # Try values around 50 to 100.
+    RESPONSE = 70
+
     def __init__(self, maestro, ch):
-        self.maestro = maestro;
-        self.ch = ch;
-        ## Init thruster accel and speed parameters
-        self.maestro.setAccel(ch, 0);
-        self.maestro.setSpeed(ch, RESPONSE);
-        #thruster min/max and center Values
-        self.min_s = MIN_SPEED;
-        self.max_s = MAX_SPEED;
-        self.center_s = CENTER_SPEED;
+        """Pass the maestro controller object and the maestro channel numbers being used for the thruster's motor controller."""
+        self.maestro = maestro
+        self.ch = ch
+        # Init thruster accel and speed parameters
+        self.maestro.setAccel(ch, 0)
+        self.maestro.setSpeed(ch, RESPONSE)
+        # thruster min/max and center Values
+        self.min_s = MIN_SPEED
+        self.max_s = MAX_SPEED
+        self.center_s = CENTER_SPEED
 
-    # Scale thruster speed(-1.0 to 1.0) to maestro servo min/center/max limits
     def ThrusterScale(self, thruster):
-        if (thruster >= 0) :
-            t = int(self.center_s + (self.max_s - self.center_s) * thruster);
+        """Scale thruster speed(-1.0 to 1.0) to maestro servo min/center/max limits."""
+        if (thruster >= 0):
+            t = int(self.center_s + (self.max_s - self.center_s) * thruster)
         else:
-            t = int(self.center_s + (self.center_s - self.min_s) * thruster);
-        return(t);
+            t = int(self.center_s + (self.center_s - self.min_s) * thruster)
+        return(t)
 
-    ## Drive thrusters given speed parameters
-    ## Valid inputs range between -1.0 and 1.0.
     def Fly(self, speed):
-        thruster = self.ThrusterScale(speed);
-        self.maestro.setTarget(self.ch, thruster);
+        """
+        Drive thrusters given speed parameters.
+
+        Valid inputs range between -1.0 and 1.0.
+        """
+        thruster = self.ThrusterScale(speed)
+        self.maestro.setTarget(self.ch, thruster)
 
     def Stop(self):
-        self.maestro.setTarget(self.ch, self.center_s);
+        """Stop the thrusters."""
+        self.maestro.setTarget(self.ch, self.center_s)
 
     def Close(self):
-        self.Stop();
+        """Stop the thrusters."""
+        self.Stop()
--- raspi/raspiComms.py
+++ raspi/raspiComms.py
@@ -1,30 +1,31 @@
+"""Communicate from server to client side."""
 import socket
 import sys
 
-#TODO: Change to topsides ip
+# TODO: Change to topsides ip
 ipSend = 'localhost'
 portSend = 5001
 ipHost = 'localhost'
 portHost = 5000
 
-#try opening a socket for communication
+# try opening a socket for communication
 try:
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 except socket.error:
-    #TODO: Change to message sent back to gui
+    # TODO: Change to message sent back to gui
     print("Failed To Create Socket")
     sys.exit()
 
-#bind the ip and port of the raspi to the socket and loop coms
+# bind the ip and port of the raspi to the socket and loop coms
 s.bind((ipHost, portHost))
 while True:
-    #receive the data from topsides
+    # receive the data from topsides
     data, addr = s.recvfrom(1024)
     data = data.decode("utf-8")
     if data == "exit":
         break
-    
-    #identify the file name and arguements
+
+    # identify the file name and arguements
     nextSpace = data.find(".py") + 3
     file = data[0:nextSpace]
     lastSpace = nextSpace + 1
@@ -35,7 +36,7 @@ while True:
         nextSpace = data.find(" ", lastSpace)
     sys.argv.append(data[lastSpace:])
 
-    #try opening and executing the file
+    # try opening and executing the file
     response = "done"
     try:
         exec(open(file).read())
