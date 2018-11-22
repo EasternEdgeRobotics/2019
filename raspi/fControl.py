228baede2954d47dfdb4f8f8190dbda996495a73
k-sutherland <krs158@mun.ca>
2018-11-21 22:54:27 -0330

    changes to get the ROV working

--- raspi/fControl.py
+++ raspi/fControl.py
@@ -4,16 +4,21 @@ sys.path.append('libraries')
 import maestro
 import control
 
+
+
 # Input through system command, the channel of the thruster
 # this input must be between and including 0-5
 tChan = int(sys.argv[1])
 # Receives speed from and including -1.0 - 1.0
 tSpeed = float(sys.argv[2])
 # 6 ports are available for thrusters.
-PORTS = [0, 1, 2, 3, 4, 5]
+# 2 1 3 5 0 4
+# 2 1 15 17 0 16
+PORTS = [2, 1, 15, 17, 0, 16]
 # Inits the maestro controller from the library.
 servo = maestro.Controller()
 # Initialize the control module with maestro channel and thruster port
-dt = control.Thruster(servo, PORTS[tChan])
+dt = control.Thruster(servo, int(PORTS[tChan])) #PORTS[tChan])
 # run the thruster
 dt.Fly(tSpeed)
+
