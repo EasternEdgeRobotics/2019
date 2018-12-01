0b531edede658851cc200f0bedd77040af9cfb8f
angtroake <angtroake@mun.ca>
2018-12-01 14:03:41 -0330

    comments

--- topsides/notificationAPI.py
+++ topsides/notificationAPI.py
@@ -9,11 +9,16 @@ notification_api.threaded = True
 lol = 0
 testNotifications = []
 
-
+"""
+returns the test page for notifications
+"""
 @notification_api.route("/testNotificationsPage")
 def loadNotificationTestPage():
     return render_template("notificationsTest.html")
 
+"""
+Returns the next notification in the test array
+"""
 @notification_api.route("/notificationTest")
 def notificationTest():
 
@@ -29,7 +34,9 @@ def notificationTest():
         
     return Response(generator(), mimetype='text/event-stream')
         
-
+"""
+adds a test notificaiton to the test notification array
+"""
 @notification_api.route("/postNotification", methods=["POST"])
 def postTestNotification():
     global lol
