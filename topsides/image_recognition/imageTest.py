5b453a04103993f272a35f8d667a7332b23d50d4
angtroake <angtroake@mun.ca>
2018-12-02 00:58:22 -0330

    Added Gstreamer OpenCV Integration

---
+++ topsides/image_recognition/imageTest.py
@@ -0,0 +1,19 @@
+import  cv2
+import numpy as np
+
+cap = cv2.VideoCapture('udpsrc port=420 ! application/x-rtp,encoding-name=H264,payload=96 ! rtph264depay ! avdec_h264 ! videoconvert ! appsink', cv2.CAP_GSTREAMER)
+
+while True:
+	
+	ret, img = cap.read()
+	
+	
+
+	cv2.imshow('test', img)
+	if cv2.waitKey(1) & 0xFF == ord('q'):
+		break
+	
+	
+
+cap.release()
+cv2.destroyAllWindows()
--- topsides/static/js/ProfileHandler.js
+++ topsides/static/js/ProfileHandler.js
@@ -1,4 +1,4 @@
-class ProfileHandle{
+class ProfileHandler{
 
     //Constructor, runs GET for profiles and stores information (asyncronously)
     constructor(){
