98458e02446c712c42ed914db4e9a3e6b40b8046
angtroake <angtroake@mun.ca>
2018-12-16 00:21:47 -0330

    push

--- topsides/TopsidesGlobals.py
+++ topsides/TopsidesGlobals.py
@@ -1,6 +1,7 @@
 import json
 
 GLOBALS = {}
+RASPI_GLOBALS = {}
 
 try:
     with open("json/Topsides.json") as file:
@@ -8,3 +9,42 @@ try:
         GLOBALS = data
 except Exception as ex:
     print("Error loading Global JSON" + str(ex))
+
+try:
+    with open("../raspi/json/Raspi.json") as file:
+        data = json.load(file)
+        RASPI_GLOBALS = data
+except Exception as ex:
+    print("Error loading Raspi Global JSON" + str(ex))
+
+
+def updateTopsidesGlobals(newData):
+    with open("json/Topsides.json", "r+") as file:
+        data = json.load(file)
+        for i in newData:
+            try:
+                data[i] = int(newData[i])
+            except:
+                data[i] = newData[i]
+
+        GLOBALS = data
+
+        file.seek(0)        
+        json.dump(data, file, indent=4)
+        file.truncate()
+
+
+def updateRaspiGlobals(newData):
+    with open("../raspi/json/Raspi.json", "r+") as file:
+        data = json.load(file)
+        for i in newData:
+            if(int(newData[i])):
+                data[i] = int(newData[i])
+            else:
+                data[i] = newData[i]
+
+        RASPI_GLOBALS = data
+
+        file.seek(0)        
+        json.dump(data, file, indent=4)
+        file.truncate()
