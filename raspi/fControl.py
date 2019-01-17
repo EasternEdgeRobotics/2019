ab1e12ac747430ceeaa886f153b0fe4caca790a4
vilinfield <vilinfield@gmail.com>
2019-01-17 19:31:28 -0330

    Test for servo control

--- raspi/fControl.py
+++ raspi/fControl.py
@@ -1,20 +1,23 @@
-"""This is a simple program to run thrusters/servo motors using the python maestro module."""
+"""Program to run the thruster and servo motors using the python maestro module."""
 import sys
 sys.path.append('libraries')
 import maestro
 import control
 
-# Input through system command, the channel of the thruster
-# this input must be between and including 0-5
+# Inputs through system command
+# The channel of the thruster from and including 0 to 9
 tChan = int(sys.argv[1])
-# Receives speed from and including -1.0 - 1.0
+# Speed from and including -1.0 to 1.0
 tSpeed = float(sys.argv[2])
-# 6 ports are available for thrusters.
-# 2 1 3 5 0 4
-# 2 1 15 17 0 16
-PORTS = [2, 1, 15, 17, 0, 16]
+# 8 ports are available for thrusters and 2 for servos
+PORTS = [0, 1, 2, 3, 7, 15, 16, 17, 8, 11]
 # Inits the maestro controller from the library.
 servo = maestro.Controller()
+
+# Change ranges for servos
+#servo.setRange(8, 1600, 10000)
+#servo.setRange(11, 1600, 10000)
+
 # Initialize the control module with maestro channel and thruster port
 dt = control.Thruster(servo, int(PORTS[tChan]))
 # Run the thruster
