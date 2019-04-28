4e91076af655dea9aa94c0ec70af19efad9fdfae
angtroake <angtroake@mun.ca>
2019-04-28 13:30:03 -0400

    3D model

--- topsides/static/js/customutil/Bot3D.js
+++ topsides/static/js/customutil/Bot3D.js
@@ -3,34 +3,37 @@ function $Bot3D(){
     //Initializing THREE.js
     var scene = new THREE.Scene();
     scene.background = new THREE.Color(0x555555);
-    var camera = new THREE.PerspectiveCamera( 10, window.innerWidth / window.innerHeight, 0.1, 1000 );
+    var camera = new THREE.PerspectiveCamera( 10, window.innerWidth / window.innerHeight, 0.1, 60 );
 
     //var texture = new THREE.CanvasTexture(canvas);
 
     var renderer = new THREE.WebGLRenderer({alpha: true});
-    renderer.setClearColor(0xffffff, 0);
-    renderer.setSize( window.innerWidth, window.innerHeight );
+    renderer.setClearColor( 0xC5C5C3 );
+    renderer.setPixelRatio( window.devicePixelRatio );
+    renderer.setSize(window.innerWidth, window.innerHeight);
 
     var botMesh;
+    var point = new THREE.Group();
+    scene.add(point);
 
-    var loader = new THREE.STLLoader();
-    loader.load("/static/models/Calypso.stl", function(geo){
-        var material = new THREE.MeshPhongMaterial({color: 0x0011ff, specular: 0x111111, shininess: 200});
-        botMesh = new THREE.Mesh(geo, material);
-        botMesh.position.set(0,0,0);
-        botMesh.scale.set(0.1,0.1,0.1);
-        botMesh.receiveShadow = true;
-        botMesh.castShadow = true;
-
-        scene.add(botMesh);
+    var loader = new THREE.GLTFLoader();
+    loader.load("/static/models/model.min.gltf", function(geo){
+        geo.scene.scale.set(0.5,0.5,0.5);
+        geo.scene.position.x = 0;
+        geo.scene.position.y = 0;
+        geo.scene.position.z = 0;
+        
+        botMesh = geo.scene;
+        botMesh.position.y = -0.0625;
+        point.add(botMesh);
     });
 
 
 
-    camera.position.z = 500;
+    camera.position.z = 3;
 
     var directionalLight = new THREE.DirectionalLight( 0xffffff, 0.5 );
-    directionalLight.position.set(0,100,300);
+    directionalLight.position.set(0,0,5);
     scene.add( directionalLight );
 
     function animate() {
@@ -45,7 +48,7 @@ function $Bot3D(){
         var socket = io.connect("/bot/telemetry");
 
         socket.on("data", function(data){
-            botMesh.rotation.set(data.accelerometer.x/(2*Math.PI),data.accelerometer.y/(2*Math.PI),data.accelerometer.y/(2*Math.PI));
+            point.rotation.set(data.accelerometer.x/(2*Math.PI),data.accelerometer.y/(2*Math.PI),data.accelerometer.y/(2*Math.PI));
         });
     }
     initSocket();
