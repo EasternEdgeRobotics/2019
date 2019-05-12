a70b4d8c3d736e00c9d2bbaa343a44e189eaab58
vilinfield <vilinfield@gmail.com>
2019-05-12 15:35:53 -0230

    Add quick set buttons (wanted values have to be added)

--- topsides/static/js/gui.js
+++ topsides/static/js/gui.js
@@ -46,7 +46,7 @@ function getPresetValue(inputAttribute, inputValue) {
 */
 function startCountDown(duration, display) {
     var timer = duration, minutes, seconds;
-      trigger = setInterval(function () {
+    trigger = setInterval(function () {
         minutes = parseInt(timer / 60, 10);
         seconds = parseInt(timer % 60, 10);
 
@@ -58,16 +58,94 @@ function startCountDown(duration, display) {
         if (--timer < 0) {
             timer = duration;
         }
-      }, 1000);
-  }
+    }, 1000);
+}
 
-  /**
-  *  Stops the countdown timer.
-  *
-  *  @param {string} display
-  *   id from gui.html to display to
-  */
-  function stopCountDown(display) {
-      display.text("00:00");
-      clearInterval(trigger);
-  }
+/**
+ *  Stops the countdown timer.
+ *
+ *  @param {string} display
+ *   id from gui.html to display to
+ */
+function stopCountDown(display) {
+    display.text("00:00");
+    clearInterval(trigger);
+}
+
+/**
+ *  Sets slider values according to quick set button.
+ *
+ *  @param {int} buttonPressed
+ *   number of button pressed
+ */
+function quickSetButtons(buttonPressed) {
+    var inputAttribute = ["surge", "heave", "sway", "yaw", "pitch", "roll"];
+    var inputValue;
+    switch (buttonPressed) {
+        case 0:
+            // Trash Rack
+            inputValue = [0, 0, 0, 0, 0, 0];
+            break;
+        case 1:
+            // Fish
+            inputValue = [0, 0, 0, 0, 0, 0];
+            break;
+        case 2:
+            // Cannon
+            inputValue = [0, 0, 0, 0, 0, 0];
+            break;
+        case 3:
+            // Pebbles
+            inputValue = [0, 0, 0, 0, 0, 0];
+            break;
+        case 4:
+            // Tire
+            inputValue = [0, 0, 0, 0, 0, 0];
+            break;
+        case 5:
+            // Micro ROV
+            inputValue = [0, 0, 0, 0, 0, 0];
+            break;
+        case 6:
+            // Benthic Species
+            inputValue = [0, 0, 0, 0, 0, 0];
+            break;
+        case 7:
+            // Line Following
+            inputValue = [0, 0, 0, 0, 0, 0];
+            break;
+        case 8:
+            // PH
+            inputValue = [0, 0, 0, 0, 0, 0];
+            break;
+        case 9:
+            // Reef Ball
+            inputValue = [0, 0, 0, 0, 0, 0];
+            break;
+        case 10:
+            // Cannon Shells
+            inputValue = [0, 0, 0, 0, 0, 0];
+            break;
+      }
+    getQuickSetValue(inputAttribute, inputValue);
+}
+
+/**
+ *  Gets the values from the quick set buttons for the server and front end display.
+ *
+ *  @param {string} inputAttribute
+ *   name of the slider axis
+ *
+ *  @param {int} inputValue
+ *   value to set each slider
+ */
+function getQuickSetValue(inputAttribute, inputValue) {
+    for(i = 0; i < inputAttribute.length; i++) {
+        inputAttr = inputAttribute[i];
+        $("#" + inputAttr).val(inputValue[i]);
+        $("#output" + inputAttr).html(inputValue[i]);
+        runPythonPOST("guislider", JSON.stringify({ slider: inputAttr, value: inputValue[i] }), function() {
+        });
+    }
+    console.log("quick set button pressed and new slider values has been sent to server");
+}
